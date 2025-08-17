4dfb65794ab3a2652d6c52f97a9c081f8efc4b8a
bkumar-27 <b.kumaraswamy.dev@gmail.com>
2025-08-17 20:29:58 +0530

    #

--- ZigRock/JS/myscript.js
+++ ZigRock/JS/myscript.js
@@ -1,5 +1,43 @@
 /*----- Jquery Code-----*/
 $(window).on('load', function () {
 	$('#preloader_status').fadeOut();
-	$('#preloader').delay(350).fadeOut();
+	$('#preloader').delay(350).fadeOut('slow');
+});
+
+
+/*-------- owl carousel --------------*/
+$(document).ready(function () {
+	$(".owl-carousel").owlCarousel({
+
+		items: 2,
+		autoplay: true,
+		smartSpeed: 750,
+		loop: true,
+		autoplayHoverPause: true,
+		dots: false,
+		nav: true,
+		navText: ['<span><i class="fas fa-angle-left"></i></span>', '<span><i class="fas fa-angle-right"></i></span>']
+	});
+});
+
+
+
+
+/*-------------- Progress Bar-------------------*/
+
+
+$(function () {
+
+	$("#progress-elements").waypoint(function () {
+		$(".progress-bar").each(function () {
+			$(this).animate({
+				width: $(this).attr("aria-valuenow") + "%"
+			}, 1000);
+		});
+
+		this.destroy();
+
+	}, {
+		offset: 'bottom-in-view'
+	});
 });
